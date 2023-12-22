54068ce87315a2a19e526430343084f97deed675
Nikita <nikondi@narod.ru>
2023-12-22 16:41:12 +0300

    Replace GET to POST in clients search

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::middleware('role:tomoru')->prefix('clients')->group(function() {
-        Route::get('search', [ClientsController::class, 'search']);
+        Route::post('search', [ClientsController::class, 'search']);
     });
     Route::middleware('role:tomoru')->prefix('requests')->group(function() {
         Route::post('/', [RequestsController::class, 'store']);
