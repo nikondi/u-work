7a098f3c6d3a9570d45da40bc259fc10e8985718
Nikita <nikondi@narod.ru>
2024-01-09 17:56:48 +0300

    Users edit and create

--- resources/js/components/Form/InputRow.jsx
+++ resources/js/components/Form/InputRow.jsx
@@ -1,13 +1,7 @@
-export default function InputRow({inputRef=null, name='', label=null, ...attributes}) {
-    if(label == null)
-        label = name;
-    if(attributes.value == null)
-        attributes.value = '';
-
-    return (
-        <label>
-            <div className="mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</div>
-            <input ref={inputRef} name={name} type="text" placeholder={label} {...attributes} className={'form-input-text'+(attributes.disabled?' opacity-70':'')} />
-        </label>
-    )
+export default function Input({inputRef=null, type = "text", name='', label=null, value, setValue = null, required = false, className = '', ...attributes}) {
+    return <input ref={inputRef} name={name} type={type} placeholder={label}
+                  onChange={(e) => setValue && setValue(e.target.value)}
+                  value={value} required={required}
+                  {...attributes}
+                  className={'form-input-text '+className+(attributes.disabled?' opacity-70':'')} />
 }
