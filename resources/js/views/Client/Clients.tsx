61cf2b8552145c5bd784ecb83f070c3c02bcb1dd
Nikita <nikondi@narod.ru>
2024-01-10 16:01:30 +0300

    Update clients

--- resources/js/views/Client/Clients.tsx
+++ resources/js/views/Client/Clients.tsx
@@ -2,7 +2,6 @@ import {TableServer} from "../../components/Table/Table.jsx";
 import React, {Fragment, useCallback, useRef, useState} from "react";
 import ClientsAPI from "../../API/ClientsAPI.js";
 import LoadingArea from "../../components/LoadingArea.jsx";
-import {Address} from "../Address/Addresses";
 import {ResourceConfig} from "../../components/Resource/hooks/useResource";
 import SearchInput from "../../components/SearchInput";
 import toast from "react-hot-toast";
@@ -88,48 +87,6 @@ export default function Clients() {
                 }}
                  setLoading={setLoading}/>
             </div>
-            {/*<Pagination list={pageList} setPage={setPage}></Pagination>
-            <Table config={{
-                columns: [
-                    {
-                        key: 'id',
-                        label: 'Номер лицевого счёта',
-                        searchable: true,
-                    },
-                    {
-                        key: 'name',
-                        label: 'ФИО',
-                        searchable: true,
-                        filter(value: string) {
-                            if(!value.trim())
-                                return (<span className="text-gray-400 dark:text-gray-500">Пусто</span>)
-                            return value;
-                        }
-                    },
-                    {
-                        key: 'address',
-                        label: 'Адрес',
-                        filter(value: Address) {
-                            if(!value)
-                                return (<span className="text-gray-400 dark:text-gray-500">Пусто</span>)
-
-                            return value.full;
-                        }
-                    },
-                    {
-                        key: 'phones',
-                        label: 'Номер телефона',
-                        filter(value: string[] | number[]) {
-                            console.log(value);
-                            if(!value || value.length == 0)
-                                return (<span className="text-gray-400 dark:text-gray-500">Пусто</span>)
-
-                            return value.map((tel:number|string, i:number) => <Fragment key={i}><a className="underline text-blue-600 dark:text-blue-300" href={'tel:'+tel}>{tel}</a>{i < value.length - 1?', ':''}</Fragment>);
-                        }
-                    }
-                ],
-                content: content,
-            }}/>*/}
         </div>
     )
 }
