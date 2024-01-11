94d4bd4daf5f1274bb4bb63641e68d13c5773df6
Nikita <nikondi@narod.ru>
2024-01-11 19:29:59 +0300

    HasRole.tsx optimize

--- resources/js/components/HasRole.tsx
+++ resources/js/components/HasRole.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren, ReactElement} from "react";
+import React, {PropsWithChildren} from "react";
 import {useStateContext} from "../contexts/ContextProvider";
 
 type HasRoleProps = {
