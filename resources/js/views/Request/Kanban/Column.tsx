a97a6dbac32b77e351ac48168f947776ec5aeb3e
Nikita <nikondi@narod.ru>
2024-01-23 16:53:14 +0300

    Paddings

--- resources/js/views/Request/Kanban/Column.tsx
+++ resources/js/views/Request/Kanban/Column.tsx
@@ -23,7 +23,7 @@ export default function KanbanColumn({type, name, colors}: KanbanListProps) {
         });
     }, []);
 
-    return <div className="w-[250px] min-w-[250px] pr-2 flex flex-col">
+    return <div className="w-[250px] min-w-[250px] px-2 flex flex-col">
         <div className={"px-3 py-2 rounded flex text-white mb-5 "+colors}><div className="flex-1">{name}</div><span className="text-gray-300">({total})</span></div>
         <div className="overflow-auto flex-1">
             {list.map((request: Request) => <KanbanItem key={request.id} item={request} colors={colors}/>)}
