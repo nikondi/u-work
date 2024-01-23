20f4fe1107c25d6bef63ad5a8f007dec7ee17702
Nikita <nikondi@narod.ru>
2024-01-23 20:46:15 +0300

    Add times icon

--- resources/js/components/Icon.tsx
+++ resources/js/components/Icon.tsx
@@ -1,7 +1,7 @@
 import React, {SVGProps} from "react"
 
 type IconProps = {
-    icon: 'phone' | 'phone-o' | 'envelope' | 'locate' | 'pencil' | 'check' | string,
+    icon: 'phone' | 'phone-o' | 'envelope' | 'locate' | 'pencil' | 'check' | 'times' | string,
 } & SVGProps<any>;
 
 export default function Icon({width = '1em', height = '1em', icon, ...props}: IconProps) {
