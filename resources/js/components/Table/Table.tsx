119f5af92d5eeadb5d55311f4f54ab6e629bc5c9
Nikita <nikondi@narod.ru>
2024-01-23 19:31:38 +0300

    SearchInput.tsx styles

--- resources/js/components/Table/Table.tsx
+++ resources/js/components/Table/Table.tsx
@@ -13,6 +13,7 @@ import {Link} from "react-router-dom";
 import useResource, {ResourceConfig} from "../Resource/hooks/useResource";
 import {twMerge} from "tailwind-merge";
 import {Pagination} from "../../views/Request/Requests";
+import SearchInput from "../SearchInput";
 
 type TableProps = {
     config: TableConfig
@@ -172,10 +173,7 @@ const TableContent = ({children, config = {}}) => {
             <div>
                 {tableConfig.buttons.map((button: ReactElement|string) => button)}
             </div>
-            <div className="w-80 relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"><svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/></svg></div>
-                <input type="search" value={searchPhrase} onChange={e => setSearchPhrase(e.target.value)} className="block w-full ps-10 bg-gray-200 dark:bg-gray-700 dark:border-gray-600 rounded-lg py-3 px-4 text-sm bg-transparent text-gray-900 bg-none border-0 focus:ring-blue-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Поиск..." required />
-            </div>
+            <SearchInput value={searchPhrase} setValue={value => setSearchPhrase(value)} />
         </div>}
         <table className="w-full text-left mt-5">
             <thead>
