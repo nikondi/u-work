c441a334baf9405601e74d334ea8fa64e9ef26fe
Nikita <nikondi@narod.ru>
2024-01-25 14:57:44 +0300

    Overflow kanban column

--- resources/js/views/Request/Kanban/Column.tsx
+++ resources/js/views/Request/Kanban/Column.tsx
@@ -18,7 +18,7 @@ export default function KanbanColumn({id, title, colors, items}: Column) {
 
   return <div className={"w-[250px] min-w-[250px] px-2 pt-2 -mt-2 flex flex-col h-full rounded transition-colors"+(overColumn?.id === id?' bg-gray-500 bg-opacity-10':'')} ref={setNodeRef}>
       <div className={"px-3 py-2 rounded flex text-white mb-5 "+colors}><div className="flex-1">{title}</div><span className="text-gray-300">({items.length})</span></div>
-      <div className="flex-1">
+      <div className="flex-1 overflow-y-scroll">
         <SortableContext id={id} items={items} strategy={rectSortingStrategy}>
           {items.map((item) => <Card id={item.id} key={item.id} item={item.content} colors={colors}/>)}
         </SortableContext>
