b3fe08ec3c1f5dee3fda8e65577f844df2664be4
Nikita <nikondi@narod.ru>
2024-01-25 13:35:41 +0300

    Styles

--- resources/js/views/Request/Kanban/Column.tsx
+++ resources/js/views/Request/Kanban/Column.tsx
@@ -16,7 +16,7 @@ export default function KanbanColumn({id, title, colors, items}: Column) {
   const { setNodeRef } = useDroppable({ id: id });
   const {overColumn} = useKanbanContext();
 
-  return <div className={"w-[250px] min-w-[250px] px-2 pt-2 -mt-2 flex flex-col h-full rounded"+(overColumn?.id === id?' bg-gray-500 bg-opacity-10':'')} ref={setNodeRef}>
+  return <div className={"w-[250px] min-w-[250px] px-2 pt-2 -mt-2 flex flex-col h-full rounded transition-colors"+(overColumn?.id === id?' bg-gray-500 bg-opacity-10':'')} ref={setNodeRef}>
       <div className={"px-3 py-2 rounded flex text-white mb-5 "+colors}><div className="flex-1">{title}</div><span className="text-gray-300">({items.length})</span></div>
       <div className="flex-1">
         <SortableContext id={id} items={items} strategy={rectSortingStrategy}>
