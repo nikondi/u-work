79329735b3efc96bbfd6717a9081590ab6309211
Nikita <nikondi@narod.ru>
2024-02-25 02:00:40 +0300

    Update Save.tsx

--- resources/js/components/Save.tsx
+++ resources/js/components/Save.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Save({children, sticky = false}) {
   return <div className="h-20">
-    <div className={"shadow-md bottom-0 left-0 right-0 p-3 bg-white dark:bg-gray-800 flex justify-end gap-x-3"+(sticky?' sticky':' absolute')}>
+    <div className={"shadow-md bottom-0 left-0 right-0 p-3 bg-white dark:bg-gray-800 flex justify-end gap-x-3 z-50"+(sticky?' sticky':' absolute')}>
       {children}
     </div>
   </div>
