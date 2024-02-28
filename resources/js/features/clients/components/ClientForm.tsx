7a30d008b0a5a38c8214bbee4d8150ebb8f18379
Nikita <nikondi@narod.ru>
2024-02-28 04:53:47 +0300

    Refactor, but not done

--- resources/js/features/clients/components/ClientForm.tsx
+++ resources/js/features/clients/components/ClientForm.tsx
@@ -1,12 +1,12 @@
 import React, {FormEventHandler, useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {clientStatuses, defaultClient, statusToNumber} from "../const";
-import {Client} from "../types";
+import {Client, ClientRaw} from "../types";
 import {ClientsAPI} from "../api";
 import {FormRow, Input, Option, Repeater, Select, Textarea} from "@/components/Form";
 import {AddressSelect} from "@/features/search_selects";
 import LoadingArea from "@/components/LoadingArea";
-import {err} from "@/helpers";
+import {err, getInputInt} from "@/helpers";
 import Save from "@/components/Save";
 
 export function ClientFormPage() {
@@ -33,7 +33,7 @@ export function ClientForm({id}) {
   const onSave: FormEventHandler = (e) => {
     e.preventDefault();
 
-    const client_data: Client & {address_id: number} = {...client, address_id: client.address.id, status: statusToNumber[client.status]};
+    const client_data: ClientRaw = {...client, password: '', address_id: client.address.id, status: statusToNumber[client.status]};
     console.log(client_data);
     setLoading(true);
 
@@ -54,49 +54,46 @@ export function ClientForm({id}) {
   return (
       <form onSubmit={onSave}>
         <div className="relative">
-          {(loading || !client)
-            ? <LoadingArea />
-            : <>
-              <div className="flex justify-between gap-x-3">
-                <h1 className="text-2xl dark:text-white">{id && <span className="text-orange-500">#{client.id} </span>}{client.name}</h1>
-              </div>
-              <FormRow label="ФИО" className="mt-4">
-                <Input label="ФИО" value={client.name} setValue={(v: string) => setClient({...client, name: v})}/>
-              </FormRow>
-              <div className="flex flex-wrap -mx-2">
-                <FormRow label="Номер лицевого счёта" className="mt-4 md:w-1/2 px-2">
-                  <Input label="Номер лицевого счёта" value={client.id} setValue={(v: number) => setClient({...client, id: v})}/>
-                </FormRow>
-                <FormRow label="Номер телефона" className="mt-4 md:w-1/2 px-2">
-                  <Repeater value={client.phone} setValue={(v: string) => setClient({...client, phone: v})} />
-                </FormRow>
-              </div>
-              <FormRow label="E-mail" className="mt-4">
-                <Input label="E-mail" value={client.email || ''} setValue={(v: string) => setClient({...client, email: v})}/>
-              </FormRow>
-              <FormRow label="Статус" className="mt-4">
-                <Select label="Статус" value={client.status}>
-                  {Object.keys(clientStatuses).map((key: string, i: number) => <Option index={i} key={i} value={key}>{clientStatuses[key]}</Option>)}
-                </Select>
-              </FormRow>
-              <FormRow label="Адрес" className="mt-4">
-                <Select value={client.address} onChange={v => setClient({...client, address: v})}>
-                  <AddressSelect address={client.address}/>
-                </Select>
-                {client.address && <a href={`/addresses/${client.address.id}`} target="_blank" className="dotted-btn">Редактировать адрес</a>}
-              </FormRow>
-              <div className="flex flex-wrap -mx-2">
-                <FormRow label="Квартира" className="mt-4 md:w-1/2 px-2">
-                  <Input label="Квартира" type="number" value={client.apartment || ''} setValue={(v: number) => setClient({...client, apartment: v})}/>
-                </FormRow>
-                <FormRow label="Этаж" className="mt-4 md:w-1/2 px-2">
-                  <Input label="Этаж" type="number" value={client.floor || ''} setValue={(v: number) => setClient({...client, floor: v})}/>
-                </FormRow>
-              </div>
-              <FormRow label="Комментарий" className="mt-4">
-                <Textarea label="Комментарий" value={client.comment || ''} setValue={(v: string) => setClient({...client, comment: v})}></Textarea>
-              </FormRow>
-            </>}
+          {loading && <LoadingArea/>}
+          <div className="flex justify-between gap-x-3">
+            <h1 className="text-2xl dark:text-white">{id && <span className="text-orange-500">#{client.id} </span>}{client.name}</h1>
+          </div>
+          <FormRow label="ФИО" className="mt-4">
+            <Input label="ФИО" value={client.name} setValue={(v) => setClient({...client, name: v})}/>
+          </FormRow>
+          <div className="flex flex-wrap -mx-2">
+            <FormRow label="Номер лицевого счёта" className="mt-4 md:w-1/2 px-2">
+              <Input label="Номер лицевого счёта" value={client.id} setValue={(v) => setClient({...client, id: getInputInt(v)})}/>
+            </FormRow>
+            <FormRow label="Номер телефона" className="mt-4 md:w-1/2 px-2">
+              <Repeater value={client.phone} setValue={(v: string) => setClient({...client, phone: v})} />
+            </FormRow>
+          </div>
+          <FormRow label="E-mail" className="mt-4">
+            <Input label="E-mail" value={client.email || ''} setValue={(v: string) => setClient({...client, email: v})}/>
+          </FormRow>
+          <FormRow label="Статус" className="mt-4">
+            <Select label="Статус" value={client.status}>
+              {Object.keys(clientStatuses).map((key: string, i: number) => <Option index={i} key={i} value={key}>{clientStatuses[key]}</Option>)}
+            </Select>
+          </FormRow>
+          <FormRow label="Адрес" className="mt-4">
+            <Select value={client.address} onChange={v => setClient({...client, address: v})}>
+              <AddressSelect address={client.address}/>
+            </Select>
+            {client.address && <a href={`/addresses/${client.address.id}`} target="_blank" className="dotted-btn">Редактировать адрес</a>}
+          </FormRow>
+          <div className="flex flex-wrap -mx-2">
+            <FormRow label="Квартира" className="mt-4 md:w-1/2 px-2">
+              <Input label="Квартира" type="number" value={client.apartment || ''} setValue={(v) => setClient({...client, apartment: getInputInt(v)})}/>
+            </FormRow>
+            <FormRow label="Этаж" className="mt-4 md:w-1/2 px-2">
+              <Input label="Этаж" type="number" value={client.floor || ''} setValue={(v) => setClient({...client, floor: getInputInt(v)})}/>
+            </FormRow>
+          </div>
+          <FormRow label="Комментарий" className="mt-4">
+            <Textarea label="Комментарий" value={client.comment || ''} setValue={(v: string) => setClient({...client, comment: v})}></Textarea>
+          </FormRow>
         </div>
         <Save>
           <button className="btn btn-primary px-3 py-2">Сохранить</button>
