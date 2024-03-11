b016986e84e3bcffd87868bd3e4fa7aa4e72c3ff
Nikita <nikondi@narod.ru>
2024-03-11 16:53:10 +0300

    Add status to resource

--- app/Http/Resources/ObjectResource.php
+++ app/Http/Resources/ObjectResource.php
@@ -5,6 +5,11 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Class ObjectResource
+ *
+ * @mixin \App\Models\Objects
+ * */
 class ObjectResource extends JsonResource
 {
     public static $wrap = false;
@@ -16,7 +21,6 @@ class ObjectResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        /* @var \App\Models\Objects $this */
         return [
             'id' => $this->id,
             'type' => $this->type,
@@ -25,6 +29,8 @@ class ObjectResource extends JsonResource
             'nets' => $this->nets?ObjectNetResource::collection($this->nets):[],
             'cameras' => $this->cameras?ObjectCameraResource::collection($this->cameras):[],
             'sip' => $this->sip,
+            'status' => $this->status,
+            'last_online' => $this->last_online,
             'minipc_model' => $this->minipc_model,
             'intercom_model' => $this->intercom_model,
             'comment' => $this->comment,
