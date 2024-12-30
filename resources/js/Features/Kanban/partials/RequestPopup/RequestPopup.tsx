63d7cea14f49a03adddbcdbabeae467649aeaa2f
nikita <nikondi@narod.ru>
2024-12-30 03:26:51 +0300

    Close on escape

--- resources/js/Features/Kanban/partials/RequestPopup/RequestPopup.tsx
+++ resources/js/Features/Kanban/partials/RequestPopup/RequestPopup.tsx
@@ -3,10 +3,17 @@ import {router, usePage} from "@inertiajs/react";
 import {Editable} from "@/Contexts/EditableContext";
 import {TKanbanPageProps} from "../../types";
 import RequestForm from "./RequestForm";
+import {useEffect} from "react";
 
 export default function RequestPopup() {
   const {request} = usePage<TKanbanPageProps>().props;
   const onClose = () => router.visit(route('kanban.index'), {preserveState: true});
+
+  useEffect(() => {
+    const onEscape = (e: KeyboardEvent) => e.key == 'Escape' && onClose();
+    window.addEventListener('keyup', onEscape);
+    return () => window.removeEventListener('keyup', onEscape);
+  }, []);
 
   return <SidePopup>
     <PopupContent>
